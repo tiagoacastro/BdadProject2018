4b52969ae815edde6471da0371698cff8cdd947a
Unknown <mariana.costa214@gmail.com>
2018-04-14 13:42:58 +0100

    Imported files

---
+++ Project-2/criar.sql
@@ -0,0 +1,137 @@
+PRAGMA foreign_keys = on;
+
+-- Person
+DROP TABLE IF EXISTS Person;
+CREATE TABLE Person (
+    id    INTEGER PRIMARY KEY,
+    name    STRING NOT NULL,
+    dateOfBirth    DATE NOT NULL,
+    nationality    INTEGER REFERENCES Nationality (id) NOT NULL
+);
+
+-- Nationality
+DROP TABLE IF EXISTS Nationality;
+CREATE TABLE Nationality (
+    id    INTEGER PRIMARY KEY,
+    name    STRING NOT NULL UNIQUE
+);
+
+-- Artist
+DROP TABLE IF EXISTS Artist;
+CREATE TABLE Artist (
+    id    INTEGER REFERENCES Person (id) PRIMARY KEY,
+    stageName    STRING
+);
+
+-- Producer
+DROP TABLE IF EXISTS Producer;
+CREATE TABLE Producer (
+    id    INTEGER REFERENCES Person (id) PRIMARY KEY,
+    alias    STRING
+);
+
+-- Album 
+DROP TABLE IF EXISTS Album;
+CREATE TABLE Album (
+    id    INTEGER PRIMARY KEY,
+    title    STRING NOT NULL,
+    releaseDate    DATE NOT NULL CHECK(releaseDate > 1910),
+    producer    INTEGER REFERENCES Producer (id) NOT NULL
+);
+
+-- Contract
+DROP TABLE IF EXISTS Contract;
+CREATE TABLE Contract (
+    id    INTEGER PRIMARY KEY,
+    dateOfSignature    DATE,
+    duration    INTEGER NOT NULL CHECK(duration < 20),
+    royalties    INTEGER NOT NULL,
+    producer    INTEGER REFERENCES Producer (id),
+    artist    INTEGER REFERENCES Artist (id),
+    CHECK (producer NOT NULL or artist NOT NULL)
+);
+
+-- RecordingStudio
+DROP TABLE IF EXISTS RecordingStudio;
+CREATE TABLE RecordingStudio (
+    id    INTEGER PRIMARY KEY,
+    address    STRING NOT NULL UNIQUE,
+    rent    INTEGER
+);
+
+-- RecordingSession
+DROP TABLE IF EXISTS RecordSession;
+CREATE TABLE RecordingSession (
+    id    INTEGER PRIMARY KEY,
+    date    DATE,
+    startingHour    TIME,
+    endingHour    TIME,
+    studio    INTEGER REFERENCES RecordingStudio (id) NOT NULL,
+    album    INTEGER REFERENCES Album (id) NOT NULL,
+    CHECK(startingHour < endingHour)
+);
+
+-- Style
+DROP TABLE IF EXISTS Style;
+CREATE TABLE Style (
+    id    INTEGER PRIMARY KEY,
+    name    STRING NOT NULL UNIQUE
+);
+
+-- MonthlySales
+DROP TABLE IF EXISTS MonthlySales;
+CREATE TABLE MonthlySales (
+    id    INTEGER PRIMARY KEY,
+    monthsAfterLaunch    INTEGER NOT NULL,
+    salesNumber    INTEGER NOT NULL,
+    album    INTEGER REFERENCES Album (id) NOT NULL
+);
+
+-- MarketingCampaign
+DROP TABLE IF EXISTS MarketingCampaign;
+CREATE TABLE MarketingCampaign (
+    id    INTEGER PRIMARY KEY,
+    startingDate    DATE,
+    endingDate    DATE,
+    album    INTEGER REFERENCES Album (id) NOT NULL
+);
+
+-- Media
+DROP TABLE IF EXISTS Media;
+CREATE TABLE Media (
+    id    INTEGER PRIMARY KEY,
+    name    STRING,
+    type    STRING NOT NULL
+);
+
+-- ArtistInSession
+DROP TABLE IF EXISTS ArtistInSession;
+CREATE TABLE ArtistInSession (
+    artist    INTEGER REFERENCES Artist (id) NOT NULL,
+    session    INTEGER REFERENCES RecordingSession (id) NOT NULL,
+    PRIMARY KEY (artist, session)
+);
+
+-- AlbumStyle
+DROP TABLE IF EXISTS AlbumStyle;
+CREATE TABLE AlbumStyle (
+    style    INTEGER REFERENCES Style (id) NOT NULL,
+    album    INTEGER REFERENCES Album (id) NOT NULL,
+    PRIMARY KEY (style, album)
+);
+
+-- MediaInCampaign
+DROP TABLE IF EXISTS MediaInCampaign;
+CREATE TABLE MediaInCampaign (
+    camapaign    INTEGER REFERENCES MarketingCampaign (id) NOT NULL,
+    media    INTEGER REFERENCES Media (id) NOT NULL,
+    PRIMARY KEY (campaign, media)
+);
+
+-- ArtistInAlbum
+DROP TABLE IF EXISTS ArtistInAlbum;
+CREATE TABLE ArtistInAlbum (
+    artist    INTEGER REFERENCES Artist (id) NOT NULL,
+    album    INTEGER REFERENCES Album (id) NOT NULL,
+    PRIMARY KEY (artist, album)
+);
---
+++ Project-2/povoar.sql
@@ -0,0 +1,188 @@
+-- Nationality
+
+INSERT INTO Nationality (id, name) VALUES (1, 'Portugal');
+INSERT INTO Nationality (id, name) VALUES (2, 'Spain');
+INSERT INTO Nationality (id, name) VALUES (3, 'United Kingdom');
+INSERT INTO Nationality (id, name) VALUES (4, 'Ireland');
+INSERT INTO Nationality (id, name) VALUES (5, 'Germany');
+INSERT INTO Nationality (id, name) VALUES (6, 'Sweden');
+INSERT INTO Nationality (id, name) VALUES (7, 'Norway');
+INSERT INTO Nationality (id, name) VALUES (8, 'Finland');
+INSERT INTO Nationality (id, name) VALUES (9, 'Australia');
+INSERT INTO Nationality (id, name) VALUES (10, 'New Zealand');
+INSERT INTO Nationality (id, name) VALUES (11, 'South Korea');
+INSERT INTO Nationality (id, name) VALUES (12, 'Japan');
+INSERT INTO Nationality (id, name) VALUES (13, 'Malaysia');
+INSERT INTO Nationality (id, name) VALUES (14, 'Philippines');
+INSERT INTO Nationality (id, name) VALUES (15, 'Italy');
+INSERT INTO Nationality (id, name) VALUES (16, 'South Africa');
+INSERT INTO Nationality (id, name) VALUES (17, 'Canada');
+INSERT INTO Nationality (id, name) VALUES (18, 'United States of America');
+INSERT INTO Nationality (id, name) VALUES (19, 'Brazil');
+INSERT INTO Nationality (id, name) VALUES (20, 'Columbia');
+
+-- Person
+
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (1, 'Joao', '01/06/1988', 1);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (2, 'Tiago', '23/07/1992', 5);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (3, 'Mariana', '02/02/1991', 14);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (4, 'Carolina', '15/03/1998', 2);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (5, 'Andre', '04/05/1980', 5);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (6, 'Nuno', '20/04/1985', 6);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (7, 'Ana', '29/10/1989', 11);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (8, 'Tiago', '07/11/1993', 10);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (9, 'Benedita', '02/12/1996', 4);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (10, 'Pedro', '04/09/1996', 9);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (11, 'Flavio', '11/09/1997', 2);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (12, 'Ricardo', '17/08/1999', 8);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (13, 'Flavio', '25/01/2000', 3);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (14, 'Irina,', '20/02/1991', 2);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (15, 'Francisca', '01/04/1992', 10);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (16, 'Vasco', '14/07/1990', 15);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (17, 'Sara', '22/08/1993', 16);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (18, 'Alexandra', '08/05/1998', 20);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (19, 'Antonio', '19/01/1998', 13);
+INSERT INTO Person (id, name, dateOfBirth, nationality) VALUES (20, 'Jose', '21/10/1999', 17);
+
+
+-- Artist
+
+INSERT INTO Artist (id, stageName) VALUES (1,' ');
+INSERT INTO Artist (id, stageName) VALUES (2,' ');
+INSERT INTO Artist (id, stageName) VALUES (3,' ');
+INSERT INTO Artist (id, stageName) VALUES (4,' ');
+INSERT INTO Artist (id, stageName) VALUES (5,' ');
+INSERT INTO Artist (id, stageName) VALUES (6,' ');
+INSERT INTO Artist (id, stageName) VALUES (7,' ');
+INSERT INTO Artist (id, stageName) VALUES (8,' ');
+INSERT INTO Artist (id, stageName) VALUES (9,' ');
+INSERT INTO Artist (id, stageName) VALUES (10,' ');
+INSERT INTO Artist (id, stageName) VALUES (11,' ');
+INSERT INTO Artist (id, stageName) VALUES (12,' ');
+INSERT INTO Artist (id, stageName) VALUES (13,' ');
+INSERT INTO Artist (id, stageName) VALUES (14,' ');
+INSERT INTO Artist (id, stageName) VALUES (15,' ');
+INSERT INTO Artist (id, stageName) VALUES (16,' ');
+INSERT INTO Artist (id, stageName) VALUES (17,' ');
+INSERT INTO Artist (id, stageName) VALUES (18,' ');
+INSERT INTO Artist (id, stageName) VALUES (19,' ');
+INSERT INTO Artist (id, stageName) VALUES (20,' ');
+
+
+-- Producer
+
+INSERT INTO Producer (id, alias) VALUES (1, ' ');
+INSERT INTO Producer (id, alias) VALUES (2, ' ');
+INSERT INTO Producer (id, alias) VALUES (3, ' ');
+INSERT INTO Producer (id, alias) VALUES (4, ' ');
+INSERT INTO Producer (id, alias) VALUES (5, ' ');
+INSERT INTO Producer (id, alias) VALUES (6, ' ');
+INSERT INTO Producer (id, alias) VALUES (7, ' ');
+INSERT INTO Producer (id, alias) VALUES (8, ' ');
+INSERT INTO Producer (id, alias) VALUES (9, ' ');
+INSERT INTO Producer (id, alias) VALUES (10, ' ');
+INSERT INTO Producer (id, alias) VALUES (11, ' ');
+INSERT INTO Producer (id, alias) VALUES (12, ' ');
+INSERT INTO Producer (id, alias) VALUES (13, ' ');
+INSERT INTO Producer (id, alias) VALUES (14, ' ');
+INSERT INTO Producer (id, alias) VALUES (15, ' ');
+INSERT INTO Producer (id, alias) VALUES (16, ' ');
+INSERT INTO Producer (id, alias) VALUES (17, ' ');
+INSERT INTO Producer (id, alias) VALUES (18, ' ');
+INSERT INTO Producer (id, alias) VALUES (19, ' ');
+INSERT INTO Producer (id, alias) VALUES (20, ' ');
+
+-- Style
+
+INSERT INTO Style (id, name) VALUES (1, 'Pop');
+INSERT INTO Style (id, name) VALUES (2, 'Rock');
+INSERT INTO Style (id, name) VALUES (3, 'Electronic');
+INSERT INTO Style (id, name) VALUES (4, 'K-Pop');
+INSERT INTO Style (id, name) VALUES (5, 'Indie');
+INSERT INTO Style (id, name) VALUES (6, 'Alternative');
+INSERT INTO Style (id, name) VALUES (7, 'Punk');
+INSERT INTO Style (id, name) VALUES (8, 'Jazz');
+INSERT INTO Style (id, name) VALUES (9, 'Instrumental');
+INSERT INTO Style (id, name) VALUES (10, 'Hip Hop');
+
+-- Album
+
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (1, ' ', '06/04/2018', 1);
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (2, ' ', '06/04/2018', 2);
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (3, ' ', '06/04/2018', 3);
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (4, ' ', '06/04/2018', 4);
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (5, ' ', '06/04/2018', 5);
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (6, ' ', '06/04/2018', 6);
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (7, ' ', '06/04/2018', 7);
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (8, ' ', '06/04/2018', 2);
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (9, ' ', '06/04/2018', 3);
+INSERT INTO Album (id, title, releaseDate, producer) VALUES (10, ' ', '06/04/2018', 4);
+
+-- Contract
+
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (1, '04/06/2017', 15, 50, 1, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (2, '04/06/2017', 15, 50, 2, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (3, '04/06/2017', 15, 50, 3, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (4, '04/06/2017', 15, 50, 4, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (5, '04/06/2017', 15, 50, 5, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (6, '04/06/2017', 15, 50, 6, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (7, '04/06/2017', 15, 50, 7, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (8, '04/06/2017', 15, 50, 8, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (9, '04/06/2017', 15, 50, 9, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (10, '04/06/2017', 15, 50, 10, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (11, '04/06/2017', 15, 50, 11, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (12, '04/06/2017', 15, 50, 12, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (13, '04/06/2017', 15, 50, 13, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (14, '04/06/2017', 15, 50, 14, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (15, '04/06/2017', 15, 50, 15, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (16, '04/06/2017', 15, 50, 16, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (17, '04/06/2017', 15, 50, 17, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (18, '04/06/2017', 15, 50, 18, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (19, '04/06/2017', 15, 50, 19, NULL);
+INSERT INTO Contract (id, dateOfSignature, duration, royalties, producer, artist) VALUES (20, '04/06/2017', 15, 50, 20, NULL);
+
+-- RecordingStudio
+
+INSERT INTO RecordingStudio (id, address, rent) VALUES (1, '123 Street', 300);
+INSERT INTO RecordingStudio (id, address, rent) VALUES (2, '123 Street', 300);
+INSERT INTO RecordingStudio (id, address, rent) VALUES (3, '123 Street', 300);
+INSERT INTO RecordingStudio (id, address, rent) VALUES (4, '123 Street', 300);
+INSERT INTO RecordingStudio (id, address, rent) VALUES (5, '123 Street', 300);
+
+-- RecordingSession
+
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+INSERT INTO RecordingSession (id, date, startingHour, endingHour, studio, album) VALUES (1, '2/3/2017', '22:00:00', '23:59:00', 1, 1);
+
+-- Monthly Sales
+
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (1, 1, 1000000, 1);
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (2, 1, 725600, 2);
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (3, 1, 82346, 3);
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (4, 1, 873, 4);
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (5, 1, 7764, 5);
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (6, 2, 8736, 1);
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (7, 2, 7637, 3);
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (8, 2, 11240, 5);
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (9, 3, 4855, 1);
+INSERT INTO MonthlySales (id, monthsAfterLaunch, salesNumber, album) VALUES (10, 3, 9323, 5);
+
+-- MarketingCampaign
+
+-- Media
+
+-- ArtistsInSession
+
+-- AlbumStyle
+
+-- MediaInCampaign
+
+-- ArtistInAlbum
