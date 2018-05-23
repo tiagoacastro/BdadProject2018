825ac0ec329e427b3fe95407fcd5b4c78f47aafe
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-05-23 16:11:28 +0100

    int 3

--- Project-3/int3.sql
+++ Project-3/int3.sql
@@ -1,3 +1,8 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+-- numero de vendas do album Recovery no primeiro mes
+select Album.title, MonthlySales.salesNumber
+from MonthlySales, Album
+where MonthlySales.album = Album.id and Album.title like "Recovery" and MonthlySales.monthsAfterLaunch = 1;
