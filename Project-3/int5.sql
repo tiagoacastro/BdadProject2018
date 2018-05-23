9ba19529034918b04ce4a106ad129ccc666eeeb9
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-05-23 16:56:15 +0100

    int 5 a dar, nao esquecer tirar o check

--- Project-3/int5.sql
+++ Project-3/int5.sql
@@ -3,7 +3,7 @@
 .nullvalue NULL
 
 --total de sales no primeiro mes de albuns do estilo POP
---select albuns.style, sum(MonthlySales.salesNumber) from(
-  select AlbumStyle.*
+select albuns.style, sum(MonthlySales.salesNumber) from
+( select AlbumStyle.*
   from AlbumStyle, Style
-  where AlbumStyle.style = Style.id and Style.name = "Pop";--) albuns, MonthlySales where MonthlySales.album = albuns.album and MonthlySales.monthsAfterLaunch = 1;
+  where AlbumStyle.style = Style.id and Style.name = "Pop") albuns, MonthlySales where MonthlySales.album = albuns.album and MonthlySales.monthsAfterLaunch = 1;
