c1ccf83f04da3af24443f7def3c9413b53e9bff3
Unknown <mariana.costa214@gmail.com>
2018-05-27 20:50:58 +0100

    Query 3 done and report added

---
+++ Project-3/int3.sql
@@ -0,0 +1,16 @@
+-- quais os artistas que gravaram em todos os estúdios - 3
+
+SELECT A.stageName, count(DISTINCT RS.studio) AS numStudios
+FROM Artist A, ArtistInSession AIS, RecordingSession RS, RecordingStudio RT
+WHERE 
+  A.id = AIS.artist
+  AND
+  RS.id = AIS.sessionNum
+  AND
+  RT.id = RS.studio
+GROUP BY A.stageName
+HAVING numStudios = (
+    SELECT count(RT1.id)
+    FROM RecordingStudio RT1
+);
+ 
