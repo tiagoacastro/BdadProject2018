003b1e8832ca4aa10a5ad19dcb90f42565dc570d
Unknown <mariana.costa214@gmail.com>
2018-05-27 16:57:57 +0100

    Query 9 done

---
+++ Project-3/int9.sql
@@ -0,0 +1,14 @@
+-- numero de albuns criados por artistas de uma determinada nacionalidade (por exemplo, Alemanha) -9
+
+SELECT N.name, A.stageName, count(AIA.album) as numAlbums
+FROM Artist A, Nationality N, Person P, ArtistInAlbum AIA
+WHERE 
+  A.id = P.id
+  AND
+  P.nationality = N.id
+  AND
+  AIA.artist = A.id
+  AND 
+  N.name = 'Germany'
+GROUP BY N.name, A.stageName;
+
